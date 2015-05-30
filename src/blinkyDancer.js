87f5ccab64d31e8087a6f7f792ef1f79380056b8
Simon <sburns77@gmail.com>
2015-05-29 21:21:46 -0500

    Refactoring of methods and added this keyword to change context.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,15 +1,16 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps){
-  var BlinkyDancer = new makeDancer(top, left, timeBetweenSteps);
-
+  var BlinkyDancer = new makeDancer(this.top, this.left, this.timeBetweenSteps);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-
-  var oldStep = this.step;
+  this.step();
 };
+
+makeBlinkyDancer.prototype = Object.create(Dancer.prototype);
+makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
  makeBlinkyDancer.prototype.step = function(){
     // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
+    // oldStep();
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
--- src/dancer.js
+++ src/dancer.js
@@ -2,6 +2,8 @@
 var makeDancer = function(top, left, timeBetweenSteps){
   //html templating/wrapper created here
   this.$node = $('<span class="dancer"></span>');
+  //assign timeBetweenSteps param to a property
+  this.timeBetweenSteps = timeBetweenSteps;
   //invokes setPosition method and passes top and left params
   this.setPosition(top, left);
   //invokes method to be called
@@ -11,13 +13,12 @@ var makeDancer = function(top, left, timeBetweenSteps){
 makeDancer.prototype.step = function () {
   //need to add binding here...
   setTimeout(this.step, this.timeBetweenSteps);
-    };
 };
 
 makeDancer.prototype.setPosition = function(top, left){
     var styleSettings = {
-      top: top,
-      left: left
+      top: this.top,
+      left: this.left
     };
     this.$node.css(styleSettings);
   };
