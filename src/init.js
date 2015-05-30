88024db5be2ea6d71600ddd832f7f59fb63a7ba0
Simon <sburns77@gmail.com>
2015-05-30 13:21:54 -0500

    CSS changes.

--- src/init.js
+++ src/init.js
@@ -2,11 +2,7 @@ $(document).ready(function(){
   $(".addDancerButton").on("click", function(event){
 
     var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
-
-    // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
-
-    // make a dancer with a random position
     var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
