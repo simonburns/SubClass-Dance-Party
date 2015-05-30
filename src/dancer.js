5e52f6fd06eaa8f889eca245430ae11e11eb4c01
Simon <sburns77@gmail.com>
2015-05-30 12:14:58 -0500

    Added init.js fix.

--- src/dancer.js
+++ src/dancer.js
@@ -1,17 +1,14 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps){
-  //html templating/wrapper created here
   this.$node = $('<span class="dancer"></span>');
-  //assign timeBetweenSteps param to a property
   this.timeBetweenSteps = timeBetweenSteps;
-  //invokes step method to be called
+  this.top = top;
+  this.left = left;
   this.step();
-  //invokes setPosition method and passes top and left params
   this.setPosition(top, left);
 };
 
-makeDancer.prototype.step = function () {
-  //need to add binding here...
+makeDancer.prototype.step = function(){
   setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 
