c679448c190583cf3e2671c0d21d1d35795a6d4b
Simon <sburns77@gmail.com>
2015-05-30 12:26:28 -0500

    Syntax fix for init.js around new dancers.

--- src/modernDancer.js
+++ src/modernDancer.js
@@ -1,20 +1,10 @@
 var makeModernDancer = function(top, left, timeBetweenSteps){
-  // var makeBlinkyDancer = new makeDancer(top, left, timeBetweenSteps);
   makeDancer.call(this,top, left, timeBetweenSteps);
-  // this.$node.addClass("blinkyDancer");
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
 };
 
 makeModernDancer.prototype = Object.create(makeDancer.prototype);
 makeModernDancer.prototype.constructor = makeModernDancer;
-// makeBlinkyDancer.prototype.oldStep = makeDancer.prototype.step;
 makeModernDancer.prototype.step = function(){
-    // call the old version of step at the beginning of any call to this new version of step
   makeDancer.prototype.step.call(this);
-    // makeDancer.prototype.step.call(this);
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
   this.$node.toggle();
   };
