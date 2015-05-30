be00bb73183f0777ed6121b2f1106ce24b66c2a4
Simon <sburns77@gmail.com>
2015-05-30 12:24:33 -0500

    Added structure for two new types of dancers.

---
+++ src/oldSchoolDancer.js
@@ -0,0 +1,10 @@
+var oldSchoolDancer = function(top, left, timeBetweenSteps){
+  makeDancer.call(this,top, left, timeBetweenSteps);
+};
+
+oldSchoolDancer.prototype = Object.create(makeDancer.prototype);
+oldSchoolDancer.prototype.constructor = oldSchoolDancer;
+oldSchoolDancer.prototype.step = function(){
+  makeDancer.prototype.step.call(this);
+  this.$node.toggle();
+  };
