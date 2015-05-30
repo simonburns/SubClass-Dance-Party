f9f9da5a919c1a82a5de4d87ebaaff77cd478a40
Simon <sburns77@gmail.com>
2015-05-30 11:35:48 -0500

    Added apply invocations to subclasses' superclass.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,5 +1,6 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps){
-  var BlinkyDancer = new makeDancer(this.top, this.left, this.timeBetweenSteps);
+  // var BlinkyDancer = new makeDancer(this.top, this.left, this.timeBetweenSteps);
+  makeDancer.apply(this, arguments);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
   this.step();
@@ -11,6 +12,7 @@ makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
  makeBlinkyDancer.prototype.step = function(){
     // call the old version of step at the beginning of any call to this new version of step
     // oldStep();
+    MakeDancer.prototype.step.apply(this, arguments);
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
